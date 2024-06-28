90638829d831cf699d851cd841e0810e1d6caf23
klei22 <klei22>
2024-06-28 13:21:33 -0700

    Add new test files

---
+++ test_agg.py
@@ -0,0 +1,22 @@
+
+import influxdb_client, os, time
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+token = os.environ.get("INFLUXDB_TOKEN")
+org = "chromebook"
+url = "http://localhost:8086"
+
+client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+query_api = client.query_api()
+
+query = """from(bucket: "chromebook_test")
+  |> range(start: -10m)
+  |> filter(fn: (r) => r._measurement == "measurement1")
+  |> mean()"""
+tables = query_api.query(query, org="chromebook")
+
+for table in tables:
+    for record in table.records:
+        print(record)
+
